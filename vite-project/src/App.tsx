75cfc935d893eb9879bc7048ef0ca22c2bbb7316
Vraj York <vraj.g@york.ie>
2025-12-17 18:54:16 +0530

    added shadncn

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Page from './app/dashboard/page'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Page/>
+    </>
+  )
+}
+
+export default App
